1c7924263ac24ccdf5c016f1ad8cdc432dee2e6d
Наталья <109668015+andre19071@users.noreply.github.com>
2022-08-16 19:38:25 +0700

    task24

--- task24/Program.cs
+++ task24/Program.cs
@@ -3,3 +3,13 @@
 //7 -> 28
 //4 -> 10
 // 8 -> 36
+
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int  i=1; i <= A; i++)
+{
+    sum = sum + i;
+}
+Console.WriteLine( $"Сумма чисел от 1 до А равна {sum}");
